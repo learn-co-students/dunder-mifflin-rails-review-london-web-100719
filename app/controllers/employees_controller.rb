57d0b336c2be07df0bb552fbd090d164cede3440
gawaxx <jules.blanc@unccub.com>
2019-11-06 16:52:31 +0000

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all 
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end 
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end 
+
+    def edit
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+    end
+
+    def create
+        employee = Employee.new(strong_params)
+        if employee.valid?
+            employee.save
+            redirect_to employee_path(employee)
+        else 
+            render :new
+        end 
+    end 
+
+    def update
+        @employee = Employee.find(params[:id])
+        @dogs = Dog.all
+        if @employee.update(strong_params)
+            redirect_to employee_path(@employee)
+        else 
+            render :edit 
+        end 
+    end 
+
+    private 
+
+    def strong_params 
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end 
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true 
+
+    validates :first_name, uniqueness: true
+    validates :last_name, uniqueness: true 
 end
