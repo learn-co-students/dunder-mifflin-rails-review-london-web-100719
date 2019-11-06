f852d4480d25f0427f9d92951a7a7a9562398135
gortron <gordon.lanza@gmail.com>
2019-11-06 15:40:36 +0000

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,52 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :edit, :update]
+  
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to @employee and return
+    else
+      redirect_to new_employee_path and return
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @employee.update(employee_params)
+    redirect_to @employee
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(
+      :first_name, 
+      :last_name, 
+      :alias,
+      :title,
+      :office,
+      :dog_id,
+      :img_url)
+  end
 end
